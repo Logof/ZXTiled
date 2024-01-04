b48505c642a3e0b55d9f99591cd324021e275c49
Кокорев Алексей Григорьевич <kokorev.alexey@otr.ru>
2024-01-05 03:15:35 +0500

    * Добавление движущихся объектов * - Выпилен calculateParallaxOffsetZoomed, calculateParallaxOffset - Объекты привязаны к сетке (создание/перетаскивание/удаление) - почищены итераторы

--- src/main/java/org/github/logof/zxtiled/mapeditor/brush/CustomBrush.java
+++ src/main/java/org/github/logof/zxtiled/mapeditor/brush/CustomBrush.java
@@ -64,7 +64,7 @@ public class CustomBrush extends AbstractBrush {
      *
      * @throws Exception
      * @see TileLayer#mergeOnto(MapLayer)
-     * @see tiled.mapeditor.brush.Brush#doPaint(int, int)
+     * @see org.github.logof.zxtiled.mapeditor.brush.Brush#doPaint(int, int)
      */
     public Rectangle doPaint(int x, int y) throws Exception {
         int layer = initLayer;
